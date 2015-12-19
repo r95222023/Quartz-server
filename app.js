a9ecd16878fe48bddfde0bab0515f8aac3503df0
r95222023 <r95222023@gmail.com>
2015-12-18 23:14:39 -0600

    add getReadyPromise method

--- app.js
+++ app.js
@@ -8,26 +8,29 @@ var app = require('./lib/expressApp'),
     serverGroup = require('./lib/servers')(config);
 
 
-auth().then(function () {
+auth()
+    .then(serverGroup.getReadyPromise)
+    .then(function (serverId) {
+        console.log('start server: '+serverId);
+        var watchList = [];
+        for (var key in watch_list) {
+            watchList.push(watch_list[key])
+        }
+        watch(watchList);
+        //execPhp('test.php').then(function(php, outprint){
+        //    console.log(outprint);
+        //    console.log(php);
+        //}, function(error){
+        //    console.log(error);
+        //})
 
-    var watchList = [];
-    for (var key in watch_list) {
-        watchList.push(watch_list[key])
-    }
-    watch(watchList);
-    //execPhp('test.php').then(function(php, outprint){
-    //    console.log(outprint);
-    //    console.log(php);
-    //}, function(error){
-    //    console.log(error);
-    //})
 
+        ///////////////////
+        var port = /*process.env.PORT || 8080*/ 3000;
+        //// use sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
+        app.listen(port, function () {
+            console.log('Server listening on: http://104.196.19.150:80')
+        });
 
-    ///////////////////
-    var port = /*process.env.PORT || 8080*/ 3000;
-    //// use sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
-    app.listen(port, function () {
-        console.log('Server listening on: http://104.196.19.150:80')
     });
 
-});
