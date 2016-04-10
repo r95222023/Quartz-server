0619b049b1ed362bdc4c0afb0779ae2157ee3d7f
Bo-Yan Huang <r95222023@gmail.com>
2016-04-09 19:33:51 -0500

    rewrite index and order-validations tasks to fit the new data structure

--- tasks/order-validations.js
+++ tasks/order-validations.js
@@ -1,25 +1,17 @@
 var defaultConfig = require('../config'),
     allpay = require('../lib/allpay'),
     firebaseUtil = require('../lib/firebaseUtil'),
+    util = require('../lib/util'),
     orderService = require('../lib/orderService');
 
 
-
-
-function delayedResolve(resolve, delay) {
-    return function () {
-        setTimeout(function () {
-            resolve();
-        }, delay || 10000);
-    }
-}
 
 function init(config){
 
     var _config=config||{},
         options = {
             'specId': 'order_validate',
-            'numWorkers': _config.numWorkers||1,
+            'numWorkers': _config.numWorkers||10,
             'sanitize': false,
             'suppressStack': _config.suppressStack||true
         },
@@ -35,13 +27,24 @@ function init(config){
             case 'allpay':
                 var os =orderService(data);
                 if (os.isValid()) {
-                    var CheckMacValue = allpay.genCheckMacValue(data.payment.allpay);
-                    firebaseUtil.ref(defaultConfig.FBURL).child('queue/tasks').child(data['_id']).child('payment/allpay/CheckMacValue').set(CheckMacValue)
-                        .then(function () {
-                            //var os =orderService(data, config)
-                            //return os.add(data)
-                        })
-                        .then(delayedResolve(resolve, 10000));
+                    var CheckMacValue = allpay.genCheckMacValue(data.payment.allpay),
+                        orderRef = rootRef.child('tasks').child(data['_id']);
+                    orderRef.child('payment/allpay/CheckMacValue').set(CheckMacValue)
+                        .then(function(){
+                            var delay = util.delayed(function(){
+                                orderRef.child('status').off();
+                                resolve();
+                            }, data.waiting||60000);
+                            
+                            orderRef.child('status').on('value', function(snap){
+                                if(snap.val()==='established'){
+                                    console.log('established');
+                                    delay.immediate();
+                                    //var os =orderService(data, config)
+                                    //return os.add(data)
+                                }
+                            })
+                        });
                 } else {
                     reject("invalid data");
                     return;
